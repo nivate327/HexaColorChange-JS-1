b93e62a5c0bc3a88bcadc1e127e252253f2e0bf9
nivate327 <rohannivate321@gmail.com>
2021-08-16 13:05:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+let main=document.querySelector("main");
+let changeBg=document.getElementById("changeBg");
+
+let arr=[
+  {
+    first:"#FFC146",
+    second:"#D9F368"
+  },
+  {
+    first: "#9AD9E9",
+    second: "#B4E555"
+  },
+  {
+    first: "#FA068A",
+    second: "#C114F8"
+  },
+  {
+    first: "#A306FA",
+    second: "#8AFA06"
+  }];
+
+changeBg.addEventListener("click", ()=>
+{
+  let random=Math.floor(Math.random() * 5);
+  
+  //console.log(random);
+  main.style.background=`linear-gradient(to right, ${arr[random].first}, ${arr[random].second})`;
+});
